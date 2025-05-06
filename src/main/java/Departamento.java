32302bacd2206c0aa5a109207da72b247ade41d7
PlinStudios <plincompany2@gmail.com>
2025-05-06 14:36:48 -0400

    Empleado y Departamento

--- src/main/java/Departamento.java
+++ src/main/java/Departamento.java
@@ -1,4 +1,17 @@
-package src.main.java;
+import java.util.ArrayList;
 
 public class Departamento {
+    private String nombre;
+    private ArrayList<Empleado> ListaEmpleados;
+
+    public Departamento(String nombre){
+        this.nombre=nombre;
+    }
+
+    public void añadirEmpleado(String apellidos, String nombre, String correo){
+        ListaEmpleados.add(new Empleado(Integer.toString(ListaEmpleados.size()),apellidos,nombre,correo));
+    }
+    public int obtenerCantidadEmpleados(){
+        return ListaEmpleados.size();
+    }
 }
--- src/main/java/Empleado.java
+++ src/main/java/Empleado.java
@@ -1,4 +1,13 @@
-package src.main.java;
-
 public class Empleado {
+    private String id;
+    private String apellidos;
+    private String nombre;
+    private String correo;
+
+    public Empleado(String id, String apellidos, String nombre, String correo){
+        this.id=id;
+        this.apellidos=apellidos;
+        this.nombre=nombre;
+        this.correo=correo;
+    }
 }
